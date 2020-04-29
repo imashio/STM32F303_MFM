b2b2f25abac5ceeae217d542fcc1ac700480f678
imashio <ims.sldc@gmail.com>
2020-04-29 12:29:31 +0900

    CAN_OBD debug
    fix include file

--- Inc/can_obd.h
+++ Inc/can_obd.h
@@ -9,4 +9,4 @@ extern volatile uint8_t         CAN_Received; // CAN data received flag
 
 void CAN_OBD_Init();
 
-void CAN_OBD_Response(uint8_t MAP, uint16_t rpm, uint8_t SPEED, uint8_t THROTTLE, uint8_t COOLANT_TEMP, uint8_t OIL_TEMP, uint16_t FUELPRESS);
+void CAN_OBD_Response(uint8_t MAP, uint16_t rpm, uint8_t Speed, uint8_t Throttle, uint8_t CoolantTemp, uint8_t OilTemp, uint16_t FuelPress, uint8_t IntakeAirTemp);
